635d76c9ae0b5bc09be7310a32c74d9636377f59
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-10-28 10:17:50 -0400

    feat: change how rook-ceph is granted permissions

--- cluster_setup/040_ceph_rook_storage/run.sh
+++ cluster_setup/040_ceph_rook_storage/run.sh
@@ -5,5 +5,11 @@ helm repo add rook-release https://charts.rook.io/release
 # TODO: Customize this installation (https://rook.io/docs/rook/v1.8/ceph-cluster-crd.html)
 helm install --create-namespace --namespace rook-ceph rook-ceph-cluster --set operatorNamespace=rook-ceph rook-release/rook-ceph-cluster
 
+# This should be done before installing the operator, but the operator creates the namespace. There
+# is probably a way to inject this as a YAML config into the operator installation but I need to
+# identify that.
+kubectl label ns rook-ceph pod-security.kubernetes.io/enforce=privileged
+
 kubectl --namespace rook-ceph get cephcluster
+
 kubectl get storageclass
