18f041b6dfd3644b5411e34ef4a709d4f573ea3c
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-10-28 11:58:10 -0400

    feat: some more work on trying to get secureboot working

--- build_steps/02_generate_custom_boot_material.sh
+++ build_steps/02_generate_custom_boot_material.sh
@@ -14,7 +14,28 @@ if [ ! -f _out/pcr-signing-key.pem ]; then
   talosctl gen secureboot pcr
 fi
 
-COMMON_IMAGER_OPTIONS="--arch amd64 --extra-kernel-arg net.ifnames=0"
+# ## Kernel Overrides:
+#
+# ### Predictable Interface Names
+#
+# Relevant entries:
+#
+# * `net.ifnames=0`
+#
+# Predictable names for the network interfaces allow us to pre-configure the roles and services
+# relying on knowing the specific interface name such as a cluster VIP. Since in our virtualized
+# environment we can guarantee there is always exactly one non-loopback network interface we don't
+# have to worry about the wrong interface being selected.
+#
+# This is notably not true in the firmament deployment plans which should omit this option.
+#
+# ### Interactive Console Dashboard
+#
+# * `talos.dashboard.disabled=1`
+#
+# Our virtual machine console's are not exposed to any form of management EXCEPT in the firmament
+# case. We do not want to leave an administrative running where we have no intention of using it.
+COMMON_IMAGER_OPTIONS="--arch amd64 --extra-kernel-arg net.ifnames=0 --extra-kernel-arg talos.dashboard.disabled=1"
 
 podman run --rm -t -v ${PWD}/_out:/secureboot:ro -v $PWD/_out:/out \
   ghcr.io/siderolabs/imager:${TALOS_VERSION} secureboot-installer \
