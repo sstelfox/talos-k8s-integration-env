e85c523fb34f043877638c5c89d788187282e715
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-10-29 00:44:07 -0400

    feat: host name resolution, internal discovery service, disable time sync

--- build_steps/10_show_cluster.sh
+++ build_steps/10_show_cluster.sh
@@ -4,5 +4,5 @@ sudo --preserve-env=HOME talosctl cluster show --provisioner qemu --name firmame
 kubectl get nodes -o wide
 talosctl health --nodes 10.5.0.2
 
-#talosctl read -n 10.5.0.2 /system/state/config.yaml > controlplane.yaml
-#talosctl read -n 10.5.0.5 /system/state/config.yaml > worker.yaml
+#talosctl read /system/state/config.yaml --cluster firmament-integration -n 10.5.0.2 > controlplane.yaml
+#talosctl read /system/state/config.yaml --cluster firmament-integration -n 10.5.0.5 > worker.yaml
