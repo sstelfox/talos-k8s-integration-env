d60dc64b6afd435feafc95dde9410ca116e4741f
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-10-30 23:03:44 -0400

    chore: bit more organization

--- scripts/firmament-diag.sh
+++ scripts/firmament-diag.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 
-sudo --preserve-env=HOME talosctl cluster show --provisioner qemu --name firmament-integration
-kubectl get nodes -o wide
+talosctl cluster show --provisioner qemu --name firmament-integration
 talosctl health --nodes 10.5.0.2
+
+kubectl get nodes -o wide
 
 #talosctl read /system/state/config.yaml --cluster firmament-integration -n 10.5.0.2 > controlplane.yaml
 #talosctl read /system/state/config.yaml --cluster firmament-integration -n 10.5.0.5 > worker.yaml
