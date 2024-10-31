6c14790ebd4a02ff6c9d65f2fe0f832a45d85e86
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-10-31 14:20:23 -0400

    fix: bit better syntax but still need the privileged security context

--- scripts/preboot-issue-kubeconfig.sh
+++ scripts/preboot-issue-kubeconfig.sh
@@ -7,4 +7,10 @@ set -o pipefail
 talosctl kubeconfig -n 10.5.0.2 -f
 
 kubectl get nodes
-kubectl get pods -A
+
+#kubectl get pods -n kube-system -l app=cilium-install
+#kubectl logs -n kube-system -l app=cilium-install
+
+#kubectl get pods -n kube-system -l k8s-app=cilium
+#kubectl get -n kube-system pod -l k8s-app=cilium -o yaml
+kubectl logs -n kube-system -l k8s-app=cilium
