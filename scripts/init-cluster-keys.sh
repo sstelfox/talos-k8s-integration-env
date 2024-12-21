ec33a6403ff257fa90f0251e3bfcd334f3374daa
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2024-12-20 21:49:02 -0500

    feat: more work on key tree work

--- scripts/init-cluster-keys.sh
+++ scripts/init-cluster-keys.sh
@@ -31,7 +31,6 @@ ensure_acl_key_exists() {
   return 0
 }
 
-# Users share the same identity across all environments
 ensure_current_user_key_exists() {
   if [ ! -r "secrets/identities/${KEY_USER_ID}.handle" ]; then
     echo "It seems like you're identity isn't currently configured in this repo, but we may have" >&2
@@ -60,6 +59,7 @@ ensure_current_user_key_exists() {
     echo "plugged in and we'll start the provisioning process. The key never leaves your device but does" >&2
     echo "require user confirmation during the process so pay attention!" >&2
     echo >&2
+
     echo "When prompted whether you are 'fine with having a separate identity' you should choose No." >&2
     echo "Under this use case with dedicated hardware keys for this purpose there is no privacy" >&2
     echo "concerns this remediates and the hardware identifier is required for our key audits." >&2
@@ -74,19 +74,31 @@ ensure_current_user_key_exists() {
       grep -oP 'age1.*' >"secrets/identities/${KEY_USER_ID}.pub"
   fi
 
-  # TODO: Should generate .sop.yaml file based on config
+  return 0
+}
 
-  # TODO: Need to warn on any acl key the new user is supposed to have access to according to the
-  # humans.acl file, as users forget to notify someone to grant them the desired level of access.
+ensure_headless_user_key_exists() {
+  if [ ! -r "secrets/identities/headless.key" ]; then
+    mkdir -p secrets/identities
+
+    local last_umask="$(umask)"
+    umask 0077
+
+    PUBLIC_KEY="$(age-keygen -o secrets/identities/headless.key 2>&1)"
+    echo "${PUBLIC_KEY}" | grep -oP 'age1.*' >"secrets/identities/headless.pub"
+
+    umask "${last_umask}"
+  fi
 
   return 0
 }
 
 ensure_service_key_exists() {
   local service="${1:-}"
+  local headless_user="${2:-}"
 
-  if [ -z "${key_name}" ]; then
-    echo 'usage: ensure_service_key_exists KEY_NAME' 2>&1
+  if [ -z "${service}" ]; then
+    echo 'usage: ensure_service_key_exists SERVICE' 2>&1
     return 1
   fi
 
@@ -98,8 +110,12 @@ ensure_service_key_exists() {
 
   if [ ! -f "${CLUSTER_SECRET_ROOT}/${service}.enc" ]; then
     mkdir -p "${CLUSTER_SECRET_ROOT}"
-    PUBLIC_KEY="$(age-keygen -o "${CLUSTER_SECRET_ROOT}/${service}.enc" 2>&1 | awk '{ print $3 }')"
-    sops encrypt -i ${CLUSTER_SECRET_ROOT}/${service}.enc
+
+    SERVICE_PRIVATE_KEY="$(age-keygen 2>"${CLUSTER_SECRET_ROOT}/${service}.pub")"
+    SERVICE_PUBLIC_KEY="$(cat "${CLUSTER_SECRET_ROOT}/${service}.pub")"
+
+    echo sops encrypt --age "${SERVICE_PUBLIC_KEY}" >"${CLUSTER_SECRET_ROOT}/${service}.enc"
+    exit 1
 
     echo "${PUBLIC_KEY}" >"${CLUSTER_SECRET_ROOT}/${service}.pub"
   fi
@@ -171,24 +187,101 @@ EOF
 }
 
 update_sops_config() {
+  local include_headless="${1:-false}"
+
+  # This script assumes its being run from the root of the repo
+  # TODO: Need to include service keys
+  # TODO Need to read in service public keys once they've been created
+  # TODO: Need to populate the config based on the various acl files
+
+  cat <<-EOF >.sops.yaml
+# YAML references are used here to make the format a easier to audit, manage, and maintain. SOPS
+# doesn't support specifying multiple AGE public keys with a list... They need to be comma
+# delimited strings but at least ignores whitespace sufficiently we can put them on different
+# lines.
+#
+# These groups and references are managed and maintained automatically.
+
+# Application engineers: For private applications running on the cluster that are managed by less
+# privileged users, this group only has access to non-infrastructure manifest secrets.
+x-app-eng: &
+  manifest-keys >-
+EOF
+
+  age1a5344plx2p7ar9f2xg4ewmkxn0sppqqm633cafth20kgxtefpeyqgm9fa3,
+  age1g3h5t4vlvqfgwdlaqqwrkss2sjzp0uy9fw2cfufrgekjdm6gk44qdc90az
+
+  # Operations: Needs access to secrets existing in all manifests. The users primarily manage
+  # infrastructure manifests but support application engineers.
+  x-operations: &
+  operations-keys >-
+  age1a5344plx2p7ar9f2xg4ewmkxn0sppqqm633cafth20kgxtefpeyqgm9fa3,
+  age1g3h5t4vlvqfgwdlaqqwrkss2sjzp0uy9fw2cfufrgekjdm6gk44qdc90az
+
+  # Root: Ultimate keys to the kingdom. Sensitive material protected with these keys should only be
+  # needed during cluster initialization and during any disaster recovery event. This include
+  # sensitive information such as the Vault root unsealing key, copies of private keys for the other
+  # services present here. Unauthorized access to this requires rotating all key material in the
+  # cluster.
+  x-root-keys: &
+  root-keys >-
+  age1a5344plx2p7ar9f2xg4ewmkxn0sppqqm633cafth20kgxtefpeyqgm9fa3
+
+  # * Ensure most specific paths are specified in this file before less specific ones
+  # * Provide the minimum access necessary for each key, keys are cheap and easy to manage. Rotating
+  #   many credentials is less fun.
+  creation_rules:
+  # Placeholder for app-engine only manifests, each individual app engineer protected file should
+  # be specified in a dedicated entry. In the future we may want to get more granular with this
+  # targeting specific users.
+  - path_regex: manifests/_reference_template/secrets\.yaml$
+  age: *app-eng-keys
+
+  # Rule for general manifest secrets, needs to be accessible by anyone writing secrets. If there
+  # was a large team working with these it would be worth getting more fine grained with the access
+  # grouping here based on responsibilities.
+  - path_regex: manifests/.*/secrets\.yaml$
+  age: *operations-keys
+
+  # These are encrypted copies of the private keys used by the automated services specified
+  # elsewhere in this file (such as the ArgoCD keys). These should only need to be retrieved during
+  # cluster initialization and during a DR event. The keys can be rotated without having access.
+  - path_regex: secrets/.*/.*\.key$
+  age: *root-keys
+  EOF
+
   return 0
 }
 
 main() {
+  local headless_user
+
+  if [ "${1:-}" = "-a" ]; then
+    headless_user="true"
+  else
+    headless_user="false"
+  fi
+
   initialize_cluster_permissions
 
-  # During initialization its important
-  ensure_current_user_key_exists
+  if [ "${headless_user}" = "true" ]; then
+    ensure_headless_user_key_exists
+  else
+    ensure_current_user_key_exists
+  fi
 
-  #ensure_acl_key_exists root
-  #ensure_acl_key_exists operations
-  #ensure_acl_key_exists app-eng
+  ensure_service_key_exists argocd ${headless_user}
+  ensure_service_key_exists breakglass ${headless_user}
+  ensure_service_key_exists cluster-seed-backups ${headless_user}
 
-  #ensure_service_key_exists argocd
-  #ensure_service_key_exists breakglass
-  #ensure_service_key_exists cluster-seed-backups
+  #ensure_acl_key_exists root ${headless_user}
+  #ensure_acl_key_exists operations ${headless_user}
+  #ensure_acl_key_exists app-eng ${headless_user}
 
-  #update_sops_config
+  #update_sops_config ${headless_user}
+
+  # TODO: Need to warn on any acl key the new user is supposed to have access to according to the
+  # humans.acl file, as users forget to notify someone to grant them the desired level of access.
 }
 
-main
+main $@
